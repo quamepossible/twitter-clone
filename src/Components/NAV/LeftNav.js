6855ed82f7cb656f89827777404ed5025246387b
Young - K <appiahkwame274@gmail.com>
2023-07-26 00:04:46 +0000

    working on home page

--- src/Components/NAV/LeftNav.js
+++ src/Components/NAV/LeftNav.js
@@ -1,7 +1,10 @@
+import {useState} from 'react';
+
 import styles from "./LeftNav.module.css";
 import homeIcon from "../../Assets/home.png";
 
 const LeftNav = () => {
+
   const navData = [["Home", "cottage", "/"], ["Explore", "search", "/"], ["Notifications", "notifications", "/"], ["Messages", "mail", "/"], ["Lists", "description", "/"], ["Bookmarks", "bookmark", "/"], ["Communities", "group", "/"], ["Verified", "verified", "/"], ["Profile", "person", "/"], ["More", "more_horiz", "/"]];
   return (
     <>
@@ -16,9 +19,9 @@ const LeftNav = () => {
         </div>
         {
           navData.map((link,i) => (
-            <div className={`${styles["nav-link"]} row`} key={i}>
-              <div className={styles["nav-icon"]}>
-                <span className="material-symbols-outlined center" style={{ fontSize: "30px" }}>{link[1]}</span>
+            <div id='nav-row' className={`${styles["nav-link"]} row`} key={i}>
+              <div className={`${styles["nav-icon"]} icons`}>
+                <span className={`${styles['nav-icon-symbols']} material-symbols-outlined center`}>{link[1]}</span>
               </div>
               <div className={`${styles["nav-desc"]}`}><span>{link[0]}</span></div>
             </div>
